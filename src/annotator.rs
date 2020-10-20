3337b2972c42266634716bc9ac37393f61280839
Daniël de Kok <me@danieldk.eu>
2020-10-20 10:08:56 +0200

    Import initial FFI

---
+++ src/annotator.rs
@@ -0,0 +1,127 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::ops::Deref;
+use std::path::Path;
+
+use conllu::graph::Sentence;
+use syntaxdot::config::{Config, PretrainConfig, TomlRead};
+use syntaxdot::encoders::Encoders;
+use syntaxdot::input::{SentenceWithPieces, Tokenize};
+use syntaxdot::model::bert::BertModel;
+use syntaxdot::tagger::Tagger;
+use tch::nn::VarStore;
+use tch::Device;
+
+use crate::AnnotatorError;
+
+/// A wrapper of `Tagger` that is `Send + Sync`.
+///
+/// Tensors are not thread-safe in the general case, but
+/// multi-threaded use is safe if no (in-place) modifications are
+/// made:
+///
+/// https://discuss.pytorch.org/t/is-evaluating-the-network-thread-safe/37802
+struct TaggerWrap(Tagger);
+
+unsafe impl Send for TaggerWrap {}
+
+unsafe impl Sync for TaggerWrap {}
+
+impl Deref for TaggerWrap {
+    type Target = Tagger;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct Annotator {
+    tagger: TaggerWrap,
+    tokenizer: Box<dyn Tokenize>,
+}
+
+impl Annotator {
+    pub fn load<P>(device: Device, config_path: P) -> Result<Self, AnnotatorError>
+    where
+        P: AsRef<Path>,
+    {
+        let r = BufReader::new(File::open(&config_path).map_err(|err| {
+            AnnotatorError::IOError(
+                format!(
+                    "Cannot open syntaxdot config file `{}`",
+                    config_path.as_ref().to_string_lossy()
+                ),
+                err,
+            )
+        })?);
+        let mut config = Config::from_toml_read(r)?;
+        config.relativize_paths(config_path)?;
+
+        let encoders = load_encoders(&config)?;
+        let tokenizer = load_tokenizer(&config)?;
+        let pretrain_config = load_pretrain_config(&config)?;
+
+        let mut vs = VarStore::new(device);
+
+        let model = BertModel::new(
+            vs.root(),
+            &pretrain_config,
+            &encoders,
+            0.0,
+            config.model.position_embeddings.clone(),
+        )?;
+
+        vs.load(&config.model.parameters)?;
+
+        vs.freeze();
+
+        let tagger = Tagger::new(device, model, encoders);
+
+        Ok(Annotator {
+            tagger: TaggerWrap(tagger),
+            tokenizer,
+        })
+    }
+
+    pub fn annotate_sentences(
+        &self,
+        sentences: &[Sentence],
+        batch_size: usize,
+    ) -> Result<Vec<SentenceWithPieces>, AnnotatorError> where {
+        let mut sentences_with_pieces = sentences
+            .iter()
+            .map(|s| self.tokenizer.tokenize(s.clone()))
+            .collect::<Vec<_>>();
+
+        // Sort sentences by length.
+        let mut sent_refs: Vec<_> = sentences_with_pieces.iter_mut().collect();
+        sent_refs.sort_unstable_by_key(|s| s.pieces.len());
+
+        // Split in batches, tag, and merge results.
+        for batch in sent_refs.chunks_mut(batch_size) {
+            self.tagger.tag_sentences(batch)?;
+        }
+
+        Ok(sentences_with_pieces)
+    }
+}
+
+pub fn load_pretrain_config(config: &Config) -> Result<PretrainConfig, AnnotatorError> {
+    Ok(config.model.pretrain_config()?)
+}
+
+fn load_encoders(config: &Config) -> Result<Encoders, AnnotatorError> {
+    let f = File::open(&config.labeler.labels).map_err(|err| {
+        AnnotatorError::IOError(
+            format!("Cannot open label file: {}", config.labeler.labels),
+            err,
+        )
+    })?;
+
+    Ok(serde_yaml::from_reader(&f)
+        .map_err(|err| AnnotatorError::LoadEncodersError(config.labeler.labels.clone(), err))?)
+}
+
+pub fn load_tokenizer(config: &Config) -> Result<Box<dyn Tokenize>, AnnotatorError> {
+    Ok(config.tokenizer()?)
+}
---
+++ src/sentences.rs
@@ -0,0 +1,94 @@
+use std::ops::Deref;
+
+use conllu::graph::Sentence;
+use conllu::token::{Token, Tokens};
+use ffi_support::{implement_into_ffi_by_delegation, implement_into_ffi_by_protobuf};
+
+pub mod proto {
+    include!(concat!(env!("OUT_DIR"), "/syntaxdot.sentence.rs"));
+}
+
+pub struct Sentences(Vec<Sentence>);
+
+impl Deref for Sentences {
+    type Target = [Sentence];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<&Token> for proto::Token {
+    fn from(token: &Token) -> Self {
+        proto::Token {
+            form: token.form().to_string(),
+            lemma: token
+                .lemma()
+                .map(ToString::to_string)
+                .unwrap_or_else(String::new),
+            upos: token
+                .upos()
+                .map(ToString::to_string)
+                .unwrap_or_else(String::new),
+            xpos: token
+                .xpos()
+                .map(ToString::to_string)
+                .unwrap_or_else(String::new),
+            features: token
+                .features()
+                .iter()
+                .map(|(attr, val)| (attr.clone(), val.clone()))
+                .collect(),
+            misc: token
+                .misc()
+                .iter()
+                .map(|(attr, val)| (attr.clone(), val.to_owned().unwrap_or_else(String::new)))
+                .collect(),
+        }
+    }
+}
+
+impl From<proto::Token> for Token {
+    fn from(token: proto::Token) -> Self {
+        // Ignore every other layer for now...
+        Token::new(token.form)
+    }
+}
+
+impl From<proto::Sentence> for Sentence {
+    fn from(sentence: proto::Sentence) -> Self {
+        sentence.tokens.into_iter().map(Into::into).collect()
+    }
+}
+
+impl From<Sentence> for proto::Sentence {
+    fn from(sentence: Sentence) -> Self {
+        proto::Sentence {
+            tokens: sentence.tokens().map(Into::into).collect(),
+            edges: vec![],
+        }
+    }
+}
+
+impl From<proto::Sentences> for Sentences {
+    fn from(sentences: proto::Sentences) -> Self {
+        Sentences(
+            sentences
+                .sentences
+                .into_iter()
+                .map(|sentence| sentence.into())
+                .collect(),
+        )
+    }
+}
+
+impl From<Sentences> for proto::Sentences {
+    fn from(sentences: Sentences) -> Self {
+        proto::Sentences {
+            sentences: sentences.0.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
+implement_into_ffi_by_protobuf!(proto::Sentences);
+implement_into_ffi_by_delegation!(Sentences, proto::Sentences);
